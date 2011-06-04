1ccde86768be4813b1485fdec38859742fa5cecd
othane <othane@gmail.com>
2011-06-04 22:02:27 +1200

    + map HID_DG_WIDTH and HEIGHT to ABS_MT_TOUCH_MAJOR and MINOR, android ~v2 may need this to work, see http://osdir.com/ml/android-platform/2010-10/msg00431.html
    Conflicts:
    
    	hid-nwriemann.c

--- hid-nwriemann.c
+++ hid-nwriemann.c
@@ -104,8 +104,16 @@ static int riemann_input_mapping(struct hid_device *hdev, struct hid_input *hi,
 				case HID_DG_CONTACTCOUNT:
 				case HID_DG_CONTACTMAX:
 				case HID_DG_TIPPRESSURE:
+					return 1;
+
 				case HID_DG_WIDTH:
+					debug("%s() - mapping WIDTH to ABS_MT_TOUCH_MAJOR\n", __func__);
+					hid_map_usage(hi, usage, bit, max, EV_ABS, ABS_MT_TOUCH_MAJOR);
+					return 1;
+
 				case HID_DG_HEIGHT:
+					debug("%s() - mapping HEIGHT to ABS_MT_TOUCH_MINOR\n", __func__);
+					hid_map_usage(hi, usage, bit, max, EV_ABS, ABS_MT_TOUCH_MINOR);
 					return 1;
 
 				case HID_DG_TIPSWITCH:
@@ -167,9 +175,21 @@ static void report_touch(struct riemann_data *rd, struct input_dev *input)
 		}
 		/* multitouch */
 		if (rd->touch[k].status & (TIPSWITCH_BIT | IN_RANGE_BIT | CONFIDENCE_BIT)) {
+			__u16	x = rd->touch[k].x; 
+			__u16	y = rd->touch[k].y;
+			__u16	w = rd->touch[k].w;
+			__u16	h = rd->touch[k].h;
+			/* android needs width and needs it to be non zero and some old holly version set w = 0 */
+			if (w == 0) 
+				w = 1;
+			if (h == 0) 
+				h = 1;
+			debug("%s() - sending multitouch event to input\n", __func__);
 			input_event(input, EV_ABS, ABS_MT_TRACKING_ID, rd->touch[k].contact_id);
-			input_event(input, EV_ABS, ABS_MT_POSITION_X, rd->touch[k].x);
-			input_event(input, EV_ABS, ABS_MT_POSITION_Y, rd->touch[k].y);
+			input_event(input, EV_ABS, ABS_MT_POSITION_X, x);
+			input_event(input, EV_ABS, ABS_MT_POSITION_Y, y);
+			input_event(input, EV_ABS, ABS_MT_TOUCH_MAJOR, w);
+			input_event(input, EV_ABS, ABS_MT_TOUCH_MINOR, h);
 		}
 		input_mt_sync(input);
 	}
